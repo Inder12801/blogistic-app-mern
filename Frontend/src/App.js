b5f77fa1a5aa1f2936d98ca76a1b30ba2e4605bc
Inderjeet Singh <inder12801@gmail.com>
2023-12-22 19:27:51 +0530

    backend/.end added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import BlogContext from "./context/BlogContext";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import BlogDetail from "./components/BlogDetail";
 import { useState } from "react";
@@ -11,7 +11,7 @@ import SignUp from "./components/SignUp";
 
 function App() {
   console.log("App rendered");
-  const API_URL = 'http://localhost:8080/blogs'
+  const API_URL = "http://localhost:8080/blogs";
   const BLOGS_KEY = "blogs_key";
   console.log("Hero rendered");
   const [blogs, setBlogs] = useState([]);
@@ -36,21 +36,21 @@ function App() {
   const deleteBlog = async (blog) => {
     const index = blogs.findIndex((b) => b._id === blog._id);
     const newBlogs = [...blogs];
-    const result = await axios.delete(API_URL + `${blog._id}`)
+    const result = await axios.delete(API_URL + `${blog._id}`);
     newBlogs.splice(index, 1);
     setBlogs(newBlogs);
-  }
+  };
 
   const registerUser = async () => {
     const result = await axios.post(API_URL, user);
     console.log(result.data);
     setUser((user) => [...user, user]);
-  }
+  };
   const getBlogsFromAPI = async () => {
-    const result = await axios.get(API_URL)
-    console.log("Data fetched from API ", result.data)
+    const result = await axios.get(API_URL);
+    console.log("Data fetched from API ", result.data);
     setBlogs(result.data);
-  }
+  };
 
   const value = {
     blogs,
@@ -68,12 +68,10 @@ function App() {
     user,
     setUser,
     registerUser,
-    API_URL
-  }
+    API_URL,
+  };
   return (
-    <BlogContext.Provider
-      value={{ ...value }}
-    >
+    <BlogContext.Provider value={{ ...value }}>
       <BrowserRouter>
         <div className="app">
           <Navbar />
@@ -85,7 +83,7 @@ function App() {
           <Footer />
         </div>
       </BrowserRouter>
-    </BlogContext.Provider >
+    </BlogContext.Provider>
   );
 }
 
